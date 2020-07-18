9c26d6a38f0795e342feab3133dda7ee6f0a50b4
alexhughesef <alex.hughes@ef.com>
2020-07-18 15:03:38 -0400

    add counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Page: React.FC = () => {
+    const [count, setCount] = useState(0);
     return (
-        <div>hello, world!</div>
+        <>
+            <div>{count}</div>
+            <button onClick={() => setCount((c) => c + 1)}>
+                +
+            </button>
+            <button onClick={() => setCount((c) => c - 1)}>
+                -
+            </button>
+        </>
     );
 };
 
